2ffede99af306a871c50ef4538036bc29764afcd
yusuf amer <impara1@gmail.com>
2012-10-15 15:54:39 +0200

    fixes

--- layouts/panels/multiple_row/stacked_8_4_4_reverse/stacked-8-4-4-reverse.tpl.php
+++ layouts/panels/multiple_row/stacked_8_4_4_reverse/stacked-8-4-4-reverse.tpl.php
@@ -10,23 +10,27 @@
  */
 ?>
 <div<?php if (!empty($css_id)) { print " id=\"$css_id\""; } ?>>
+    <div class="grid-row">
+    <div class="grid-full">
+      <div class="grid-inner"><?php print $content['grid_16_top_full']; ?></div>
+    </div>
   <div class="grid-row">
     <div class="grid-12-left">
-      <div class="grid-inner"><?php print $content['grid_12_top_left']; ?></div>
+      <div class="grid-inner"><?php print $content['grid_12_left']; ?></div>
     </div>
-    <aside class="grid-4-right">
-      <div class="grid-inner"><?php print $content['grid_4_top_right']; ?></div>
-    </aside>
-  </div>
-  <div class="grid-row grid-content last">
-    <div class="grid-8-left">
-      <div class="grid-inner"><?php print $content['grid_8_left']; ?></div>
-    </div>
-    <aside class="grid-4-center-right">
-      <div class="grid-inner"><?php print $content['grid_4_center_right']; ?></div>
-    </aside>
     <aside class="grid-4-right">
       <div class="grid-inner"><?php print $content['grid_4_right']; ?></div>
+    </aside>
+  </div>
+  <div class="grid-row last">
+    <div class="grid-8-left">
+      <div class="grid-inner"><?php print $content['grid_8_bottom_left']; ?></div>
+    </div>
+    <aside class="grid-4-center-right">
+      <div class="grid-inner"><?php print $content['grid_4_bottom_center_right']; ?></div>
+    </aside>
+    <aside class="grid-4-right">
+      <div class="grid-inner"><?php print $content['grid_4_bottom_right']; ?></div>
     </aside>
   </div>
 </div>
