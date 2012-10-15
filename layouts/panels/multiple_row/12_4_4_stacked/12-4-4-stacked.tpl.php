31bc291061fe83e191bf6178dc221d7e65b7aeff
yusuf amer <impara1@gmail.com>
2012-10-15 14:36:38 +0200

    fixes

---
+++ layouts/panels/multiple_row/12_4_4_stacked/12-4-4-stacked.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @file
+ * Latto implementation to present a Panels layout.
+ *
+ * Available variables:
+ * - $content: An array of content, each item in the array is keyed to one
+ *   panel of the layout.
+ * - $css_id: unique id if present.
+ */
+?>
+<div<?php if (!empty($css_id)) { print " id=\"$css_id\""; } ?>>
+  <div class="grid-row">
+    <div class="grid-full">
+      <div class="grid-inner"><?php print $content['grid_12_left']; ?></div>
+    </div>
+  </div>
+  <div class="grid-row grid-content last">
+    <div class="grid-8-left">
+      <div class="grid-inner"><?php print $content['grid_8_left']; ?></div>
+    </div>
+    <aside class="grid-4-center-right">
+      <div class="grid-inner"><?php print $content['grid_4_center_right']; ?></div>
+    </aside>
+    <aside class="grid-4-right">
+      <div class="grid-inner"><?php print $content['grid_4_right']; ?></div>
+    </aside>
+  </div>
+</div>
