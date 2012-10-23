1ef0efb2620c9547ec3529744efc25900eeb009e
yusuf amer <impara1@gmail.com>
2012-10-23 11:08:12 +0200

    fixes

--- templates/node--ding-library.tpl.php
+++ templates/node--ding-library.tpl.php
@@ -89,7 +89,7 @@
   <h1 class="page-title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h1>
 
   <div class="content"<?php print $content_attributes; ?>>
-    <?php print render($content); ?>
+    <div class="libraries"><?php print render($content); ?></div>
     <?php if (!empty($opening_hours)) { print $opening_hours; } ?>
   </div>
 </div>
