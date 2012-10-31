88b7558904b558a78dd18aaef1fd07f35f087967
yusuf amer <impara1@gmail.com>
2012-10-31 15:55:05 +0100

    fixes

--- js/base.js
+++ js/base.js
@@ -1,7 +1,7 @@
 (function($){
 
-  $(document).ready(function(){
-    //$('.search-login .grid-inner').equalize('height');
+  $(document).ready(function($) {
+    $('.search-login .grid-inner').equalize('height');
     $('input[placeholder],textarea[placeholder]').placeholder();
 
 });
