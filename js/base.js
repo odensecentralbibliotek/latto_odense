8090beda844c83f59793806a99ab84e073481cf2
yusuf amer <impara1@gmail.com>
2012-11-13 09:18:47 +0100

    fixes

--- js/base.js
+++ js/base.js
@@ -3,7 +3,8 @@
   $(document).ready(function($) {
     $('.search-login .grid-inner').equalize('height');
     $('input[placeholder],textarea[placeholder]').placeholder();
-
+        count = $('.ting-search-amount-block em:eq(2)').clone().addClass('navcount');
+    $('.nav li a').first().append(count);
 });
 
 })(jQuery);
