5a63d058ca38acbc01f6b0345e441916114140a9
yusuf amer <impara1@gmail.com>
2013-01-17 12:14:27 +0100

    Login icons paddings, added facetbrowser waiting overlay on search request

--- js/base.js
+++ js/base.js
@@ -1,15 +1,21 @@
 (function($){
-
+ 
   $(document).ready(function($) {
+   
     //$('.search-login .grid-inner').equalize('height');
     $('footer .grid-inner').equalize('height');
     $('input[placeholder],textarea[placeholder]').placeholder();
     if ($('.ting-search-amount-block')) {
-     count = $('.ting-search-amount-block em:eq(2)').clone().addClass('navcount');
-     $('.nav li a').first().append(count);
+      count = $('.ting-search-amount-block em:eq(2)').clone().addClass('navcount');
+      $('.nav li a').first().append(count);
     }
     $("a.infomedia_group").addClass("btn");
+    
     $(".field-type-ting-details-uri a").addClass("btn").text('Hent online');
-});
+    
+    $(".pane-search-form input[type=submit], .ui-corner-all").click(function(){     
+      $('<div class="facetbrowser_overlay"><div class="spinner"></div></div>').prependTo('body');
+    });
+  });
 
 })(jQuery);
