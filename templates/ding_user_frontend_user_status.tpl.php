0f149bbaa3c70b2699b98cb7b7c8b115f1d29305
yusuf amer <impara1@gmail.com>
2013-01-21 14:54:01 +0100

    Switched logged in menus

--- templates/ding_user_frontend_user_status.tpl.php
+++ templates/ding_user_frontend_user_status.tpl.php
@@ -7,25 +7,25 @@
 <div id="bruger-wrapper">
   <h2><?php print t('User menu'); ?></h2>
   <div>
-  <span><?php print l(t('My account'), 'user/'); ?></span>
+    <span ><?php print l(t('Loans:'), 'user/' . $user->uid . '/status'); ?></span>
+    <span class="label">(<?php print $loan_count; ?>)</span>
   </div>
   <div>
-  <span><?php print l(t('Logout'), 'user/logout/'); ?></span>
+    <span><?php print l(t('Reservations:'), 'user/' . $user->uid . '/status/reservations'); ?></span>
+    <span class="label">(<?php print $reservation_count; ?>)</span>
   </div>
   <div>
-  <span ><?php print l(t('Loans:'), 'user/' . $user->uid . '/status'); ?></span>
-  <span class="label">(<?php print $loan_count; ?>)</span>
+    <span><?php print l(t('Debts:'), 'user/' . $user->uid . '/status/debts'); ?></span>
+    <span class="label">(<?php print $debt_count; ?>)</span>
   </div>
   <div>
-  <span><?php print l(t('Reservations:'),'user/' . $user->uid . '/status/reservations'); ?></span>
-  <span class="label">(<?php print $reservation_count; ?>)</span>
+    <span><?php print l(t('Bookmarks:'), 'user/' . $user->uid . '/bookmarks'); ?></span>
+    <span class="label">(<?php print $bookmark_count; ?>)</span>
   </div>
   <div>
-  <span><?php print l(t('Debts:'), 'user/' . $user->uid . '/status/debts'); ?></span>
-  <span class="label">(<?php print $debt_count; ?>)</span>
+    <span><?php print l(t('My account'), 'user/'); ?></span>
   </div>
   <div>
-  <span><?php print l(t('Bookmarks:'), 'user/' . $user->uid . '/bookmarks'); ?></span>
-  <span class="label">(<?php print $bookmark_count; ?>)</span>
+    <span><?php print l(t('Logout'), 'user/logout/'); ?></span>
   </div>
 </div>
