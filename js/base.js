2b9e5017f2494dd84d7a0e3e3b0fb3a3b3228695
yusuf amer <impara1@gmail.com>
2013-02-26 15:19:23 +0100

    Bigger order-id font-size and fixes amount div. fixed ding_user_frontend_user_status to use default id's.

--- js/base.js
+++ js/base.js
@@ -5,7 +5,7 @@
     var alle = $('.select-all input[type=checkbox]').attr('title');
     $('<span> '+alle+'</span>').appendTo('.select-all');
     //$('.search-login .grid-inner').equalize('height');
-    $('footer .grid-inner').equalize('height');
+    
     $('input[placeholder],textarea[placeholder]').placeholder();
     if ($('.ting-search-amount-block')) {
       count = $('.ting-search-amount-block em:eq(2)').clone().addClass('navcount');
@@ -25,10 +25,11 @@ if (jQuery.browser.msie && jQuery.browser.version <= 8) {
         $('.ting-cover').removeClass('ting-cover');
   }
       $(window).load(function(){  
+        $('footer .grid-inner').equalize('height');
     //initialize after images are loaded  
      $('.ting-cover img').parent().removeClass('ting-cover');
   });  
     
-  }); 
+  });
 
 })(jQuery);
--- templates/ding_user_frontend_user_status.tpl.php
+++ templates/ding_user_frontend_user_status.tpl.php
@@ -6,21 +6,21 @@
 ?>
 <div id="bruger-wrapper">
   <h2><?php print t('User menu'); ?></h2>
-  <div>
+  <div id="ding-user-loan-amount">
     <span ><?php print l(t('Loans:'), 'user/' . $user->uid . '/status'); ?></span>
-    <span class="label">(<?php print $loan_count; ?>)</span>
+    <span class="label amount">(<?php print $loan_count; ?>)</span>
   </div>
-  <div>
+  <div id="ding-user-reservation-amount">
     <span><?php print l(t('Reservations:'), 'user/' . $user->uid . '/status/reservations'); ?></span>
-    <span class="label">(<?php print $reservation_count; ?>)</span>
+    <span class="label amount">(<?php print $reservation_count; ?>)</span>
   </div>
-  <div>
+  <div id="ding-user-debt-amount">
     <span><?php print l(t('Debts:'), 'user/' . $user->uid . '/status/debts'); ?></span>
-    <span class="label">(<?php print $debt_count; ?>)</span>
+    <span class="label amount">(<?php print $debt_count; ?>)</span>
   </div>
-  <div>
+  <div id="ding-user-bookmark-amount">
     <span><?php print l(t('Bookmarks:'), 'user/' . $user->uid . '/bookmarks'); ?></span>
-    <span class="label">(<?php print $bookmark_count; ?>)</span>
+    <span class="label amount">(<?php print $bookmark_count; ?>)</span>
   </div>
   <div>
     <span><?php print l(t('My profile'), 'user/'); ?></span>
