b5229a73a1dc32473826ca5c9cc4a6cd78adf0f2
yusuf amer <impara1@gmail.com>
2013-05-31 10:53:44 +0200

    Styled login box, show reservation pause.

--- templates/ding_user_frontend_user_status.tpl.php
+++ templates/ding_user_frontend_user_status.tpl.php
@@ -6,26 +6,24 @@
 ?>
 <div id="bruger-wrapper">
   <h2><?php print t('User menu'); ?></h2>
+  <div id="ding-user-profile">
+    <span><?php print l(t('My profile'), 'user/'); ?></span>
+    <span style="float:right;"><?php print l(t('Logout'), 'user/logout/'); ?></span>
+  </div>
   <div id="ding-user-loan-amount">
-    <span ><?php print l(t('Loans:'), 'user/' . $user->uid . '/status'); ?></span>
     <span class="label amount">(<?php print $loan_count; ?>)</span>
+    <span ><?php print l(t('Loans:'), 'user/' . $user->uid . '/status'); ?></span>
   </div>
   <div id="ding-user-reservation-amount">
-    <span><?php print l(t('Reservations:'), 'user/' . $user->uid . '/status/reservations'); ?></span>
     <span class="label amount">(<?php print $reservation_count; ?>)</span>
+    <span><?php print l(t('Reservations:'), 'user/' . $user->uid . '/status/reservations'); ?></span>
   </div>
   <div id="ding-user-debt-amount">
-    <span><?php print l(t('Debts:'), 'user/' . $user->uid . '/status/debts'); ?></span>
     <span class="label amount">(<?php print $debt_count; ?>)</span>
+    <span><?php print l(t('Debts:'), 'user/' . $user->uid . '/status/debts'); ?></span>
   </div>
   <div id="ding-user-bookmark-amount">
-    <span><?php print l(t('Bookmarks:'), 'user/' . $user->uid . '/bookmarks'); ?></span>
     <span class="label amount">(<?php print $bookmark_count; ?>)</span>
-  </div>
-  <div>
-    <span><?php print l(t('My profile'), 'user/'); ?></span>
-  </div>
-  <div>
-    <span><?php print l(t('Logout'), 'user/logout/'); ?></span>
+    <span><?php print l(t('Bookmarks:'), 'user/' . $user->uid . '/bookmarks'); ?></span>
   </div>
 </div>
