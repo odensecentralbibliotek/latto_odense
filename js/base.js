9e3fcd75b3aece3733fcbae03fd136e7024e945a
gorkha <anme@odense.dk>
2014-01-20 15:42:47 +0100

    Added borgerservice online booking.

--- js/base.js
+++ js/base.js
@@ -1,5 +1,11 @@
+var obq_isSSL = ("https:" == document.location.protocol);
+var obq_app = 'odensebib';
+var obq_file = 'online-booking-blue.png';
+var obq_widget_url = document.location.protocol + "//widget.onlinebooq.dk/Widget/";
+document.write(unescape("%3Cscript src='" + obq_widget_url + "onlinebooq.js' type='text/javascript'%3E%3C/script%3E"));
+      
 (function($){
-  
+
  jQuery.fn.exists = function(){
    return this.length>0;
  }
