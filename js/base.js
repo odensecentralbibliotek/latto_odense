3a96598ac5facf1bed3e8ec780bb2c24e20c697d
gorkha <anme@odense.dk>
2014-01-20 16:04:21 +0100

    fixed borgerservice online booking.

--- js/base.js
+++ js/base.js
@@ -1,8 +1,9 @@
-var obq_isSSL = ("https:" == document.location.protocol);
-var obq_app = 'odensebib';
-var obq_file = 'online-booking-blue.png';
-var obq_widget_url = document.location.protocol + "//widget.onlinebooq.dk/Widget/";
-document.write(unescape("%3Cscript src='" + obq_widget_url + "onlinebooq.js' type='text/javascript'%3E%3C/script%3E"));
+    var obq_isSSL = ("https:" == document.location.protocol);
+    var obq_app = 'odensebib';
+    var obq_file = 'online-booking-blue.png';
+    var obq_widget_url = obq_isSSL ? "https://widget.onlinebooq.dk/Widget/" : "http://widget.onlinebooq.dk/Widget/";
+    document.write(unescape("%3Cscript src='" + obq_widget_url + "onlinebooq.js' type='text/javascript'%3E%3C/script%3E"));
+
       
 (function($){
 
