b42932f5659887c3ba2dac2ae60ab00d7876e889
yusuf <maa@odense.dk>
2014-02-17 12:14:26 +0100

    Revert "Fixed styled place2book for view list."
    This reverts commit 033132c9466d80407f5b464682f73c2c49589943.

--- js/base.js
+++ js/base.js
@@ -121,6 +121,5 @@
   };
   $(document).ajaxComplete(function() {
     $(".list-item .btn-large:contains('Bestil billet(ter)')").remove();
-    $(".list-item .btn-large:contains('Køb billet(ter)')").remove();
   });
 })(jQuery);
