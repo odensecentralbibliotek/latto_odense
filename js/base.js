b7b60ac6ab5da26798f8ace934b9bf79e4677147
Amer <impara1@gmail.com>
2014-04-28 11:19:53 +0200

    Added target _blank for form id "subunsubform" because  the htmlpurifier does not allow.

--- js/base.js
+++ js/base.js
@@ -5,6 +5,7 @@
  }
 
   $(document).ready(function($) {
+      $("#subunsubform").attr("target", "_blank")
     if($('#search_input').exists()){
     $('#search_input').fastLiveFilter('.fastfilter');
     }
