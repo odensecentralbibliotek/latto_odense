65966957f6e60cf1f4b3385eff78414b98db2a68
Tobias F. Petersen <tobsf@hotmail.com>
2015-05-06 10:32:55 +0200

    integratet settings from backend.

--- js/gtranslation.js
+++ js/gtranslation.js
@@ -5,14 +5,25 @@
  */
 jQuery(document).ready(function(){
     //Add the link to top of page.
-    CloseTranslationDialogHooks = false;
-    var TranslateLink = jQuery("#page").children();
-    TranslateLink.first().prepend('<div id="Translation_Menu_Addon" class="grid-inner" style=""><a onclick="ShowTranslationDialog()" >Translate</a>\n\
+    //CloseTranslationDialogHooks = false;
+    if(typeof Drupal.settings.oc_template_overwrites !== 'undefined' &&
+           Drupal.settings.oc_template_overwrites.google_translate_enabled == 1 )
+    {
+        var TranslateLink = jQuery("#page").children();
+        TranslateLink.first().prepend('<div id="Translation_Menu_Addon" class="grid-inner" style=""><a onclick="ShowTranslationDialog()" >'+ Drupal.t('Translate')+'</a>\n\
                                    </div><div onclick="" id="Translation_dialog" title="Vælg Sprog" style="display:none;"><div id="google_translate_element"></div></div>');
+    }
+    
 });
 //Callback after google translate init.
 function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'da', layout: google.translate.TranslateElement.InlineLayout.SIMPLE,autoDisplay: false, gaTrack: true, gaId: 'UA-3030685-7'}, 'google_translate_element');  
+    var languages = '';
+    try {
+        languages = Drupal.settings.oc_template_overwrites.google_translate_languages;
+    } catch (e) {
+        
+    }  
+    new google.translate.TranslateElement({pageLanguage: 'da', includedLanguages: languages, layout: google.translate.TranslateElement.InlineLayout.SIMPLE,autoDisplay: false, gaTrack: true, gaId: 'UA-3030685-7'}, 'google_translate_element');  
 }
 function ShowTranslationDialog(e)
 {
