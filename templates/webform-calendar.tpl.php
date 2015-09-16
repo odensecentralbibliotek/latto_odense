4e0211b68fb49d012ec1080353a79ca3428eeb23
Tobias F. Petersen <tobsf@hotmail.com>
2015-09-16 09:31:56 +0200

    Updatet webform-date template to allow date format change to danish format. see: https://www.drupal.org/node/2345155 for more information.

--- templates/webform-calendar.tpl.php
+++ templates/webform-calendar.tpl.php
@@ -1,4 +1,45 @@
 <?php
- $idKey = str_replace('_', '-', $component['form_key']); 
+/**
+* This replaces the webform date selectors with a single text box & date popup.
+*/
+/** Recursive function to return an array containing form_keys of component and all parent components **/
+
+function getFormKeys($components, $component) {
+  $myReturn = array();
+  $pid = $component['pid'];
+  if(!empty($pid)) {
+    $myReturn += getFormKeys($components, $components[$pid]);
+  }// end of if(!empty($pid));
+  $myReturn[] = $component['form_key'];
+  return $myReturn;
+}// end of function getFormKeys($components, $component);
+$node = node_load($component['nid']);
+$form_keys = getFormKeys($node->webform['components'], $component);
+$form_key = join('-', $form_keys);
+$form_key = str_replace('_', '-', $form_key);
 ?>
-<input type="text" id="edit-submitted-<?php print $idKey ?>" class="form-text <?php print implode(' ', $calendar_classes); ?>" alt="<?php print t('Open popup calendar'); ?>" title="<?php print t('Open popup calendar'); ?>" />
+<input type="text" id="edit-submitted-<?php print $form_key ?>" class="form-text <?php print implode(' ', $calendar_classes); ?>" alt="<?php print t('Open popup calendar'); ?>" title="<?php print t('Open popup calendar'); ?>" />
+<script type="text/JavaScript">
+(function ($) {
+  $('#edit-submitted-<?php print $form_key; ?>').datepicker({
+    changeMonth: true,
+    changeYear: true,
+    dateFormat: 'dd-mm-yy',
+    onSelect: function() {
+      var mydate = $('#edit-submitted-<?php print $form_key; ?>').datepicker("getDate");
+      $('#edit-submitted-<?php print $form_key; ?>-day').val(mydate.getDate());
+      $('#edit-submitted-<?php print $form_key; ?>-year').val(mydate.getFullYear());
+      $('#edit-submitted-<?php print $form_key; ?>-month').val(mydate.getMonth() + 1);
+    }
+  });
+  var myYear = $('#edit-submitted-<?php print $form_key; ?>-year').val();
+  var myMonth = $('#edit-submitted-<?php print $form_key; ?>-month').val();
+  var myDate = $('#edit-submitted-<?php print $form_key; ?>-day').val();
+  if((myYear != '') && (myMonth != '') && (myDate != '')) {
+    var pad = "00";
+    var paddedMonth = pad.substring(0, pad.length - myMonth.length) + myMonth;
+    var paddedDate = pad.substring(0, pad.length - myDate.length) + myDate;
+    $('#edit-submitted-<?php print $form_key; ?>').val(myYear + '-' + paddedMonth + '-' + paddedDate);
+  }
+})(jQuery);
+</script>
