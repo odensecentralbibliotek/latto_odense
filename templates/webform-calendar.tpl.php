fd4d2a6b7d4daf2879f84a313d0ce152ca52e99a
Tobias F. Petersen <tobsf@hotmail.com>
2015-09-16 14:44:13 +0200

    multi field support. eu date format support.

--- templates/webform-calendar.tpl.php
+++ templates/webform-calendar.tpl.php
@@ -3,7 +3,6 @@
 * This replaces the webform date selectors with a single text box & date popup.
 */
 /** Recursive function to return an array containing form_keys of component and all parent components **/
-// end of function getFormKeys($components, $component);
 $node = node_load($component['nid']);
 $form_keys = getFormKeys($node->webform['components'], $component);
 $form_key = join('-', $form_keys);
@@ -12,25 +11,58 @@ $form_key = str_replace('_', '-', $form_key);
 <input type="text" id="edit-submitted-<?php print $form_key ?>" class="form-text <?php print implode(' ', $calendar_classes); ?>" alt="<?php print t('Open popup calendar'); ?>" title="<?php print t('Open popup calendar'); ?>" />
 <script type="text/JavaScript">
 (function ($) {
-  $('#edit-submitted-<?php print $form_key; ?>').datepicker({
-    changeMonth: true,
-    changeYear: true,
-    dateFormat: 'dd-mm-yy',
-    onSelect: function() {
-      var mydate = $('#edit-submitted-<?php print $form_key; ?>').datepicker("getDate");
-      $('#edit-submitted-<?php print $form_key; ?>-day').val(mydate.getDate());
-      $('#edit-submitted-<?php print $form_key; ?>-year').val(mydate.getFullYear());
-      $('#edit-submitted-<?php print $form_key; ?>-month').val(mydate.getMonth() + 1);
+  //Get start dates from web form
+  
+   $('div.webform-datepicker').each(function() {
+    var $webformDatepicker = $(this);
+    var $calendar = $webformDatepicker.find('input.webform-calendar');
+
+    // Ensure the page we're on actually contains a datepicker.
+    if ($calendar.length == 0) { 
+      return;
     }
+
+    var startDate = $calendar[0].className.replace(/.*webform-calendar-start-(\d{4}-\d{2}-\d{2}).*/, '$1').split('-');
+    var endDate = $calendar[0].className.replace(/.*webform-calendar-end-(\d{4}-\d{2}-\d{2}).*/, '$1').split('-');
+    var firstDay = $calendar[0].className.replace(/.*webform-calendar-day-(\d).*/, '$1');
+    // Convert date strings into actual Date objects.
+    startDate = new Date(startDate[0], startDate[1] - 1, startDate[2]);
+    endDate = new Date(endDate[0], endDate[1] - 1, endDate[2]);
+
+    // Ensure that start comes before end for datepicker.
+    if (startDate > endDate) {
+      var laterDate = startDate;
+      startDate = endDate;
+      endDate = laterDate;
+    }
+
+    var startYear = startDate.getFullYear();
+    var endYear = endDate.getFullYear();
+
+    // Set up the jQuery datepicker element.
+    $calendar.datepicker({
+      dateFormat: 'dd-mm-yy',
+      yearRange: startYear + ':' + endYear,
+      firstDay: parseInt(firstDay),
+      minDate: startDate,
+      maxDate: endDate,
+      onSelect: function(dateText, inst) {
+          debugger;
+        var date = dateText.split('-');
+        $webformDatepicker.find('select.year, input.year').val(+date[2]).trigger('change');
+        $webformDatepicker.find('select.month').val(+date[1]).trigger('change');
+        $webformDatepicker.find('select.day').val(+date[0]).trigger('change');
+      },
+      beforeShow: function(input, inst) {
+    
+      }
+    });
+
+    // Prevent the calendar button from submitting the form.
+    $calendar.click(function(event) {
+      $(this).focus();
+      event.preventDefault();
+    });
   });
-  var myYear = $('#edit-submitted-<?php print $form_key; ?>-year').val();
-  var myMonth = $('#edit-submitted-<?php print $form_key; ?>-month').val();
-  var myDate = $('#edit-submitted-<?php print $form_key; ?>-day').val();
-  if((myYear != '') && (myMonth != '') && (myDate != '')) {
-    var pad = "00";
-    var paddedMonth = pad.substring(0, pad.length - myMonth.length) + myMonth;
-    var paddedDate = pad.substring(0, pad.length - myDate.length) + myDate;
-    $('#edit-submitted-<?php print $form_key; ?>').val(myYear + '-' + paddedMonth + '-' + paddedDate);
-  }
 })(jQuery);
 </script>
