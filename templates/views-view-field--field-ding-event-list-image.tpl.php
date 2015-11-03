4d441bb951956a3c4c084482593942e7983d6769
Tobias F. Petersen <tobsf@hotmail.com>
2015-11-03 08:37:05 +0100

    Add support for image provider text.

---
+++ templates/views-view-field--field-ding-event-list-image.tpl.php
@@ -0,0 +1,25 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+if($output != "" &&
+isset($row->field_field_ding_event_list_image[0]['raw']['field_kilde']['und'][0]['value']))
+{
+    $dom = new DOMDocument();
+    $dom->loadHTML($output);
+
+    foreach ($dom->getElementsByTagName('img') as $item) {
+        $photo_source = $row->field_field_ding_event_list_image[0]['raw']['field_kilde']['und'][0]['value'];
+        $item->setAttribute('title', t('Photo: ') . $photo_source);
+        $item->setAttribute('alt', t('Photo: ') . $photo_source);
+        echo $dom->saveHTML();
+        break;
+    }
+}
+else
+{
+    echo $output;
+}
