530e23d2d38caad726cbe4f3ef225102090e8961
Tobias F. Petersen <tobsf@hotmail.com>
2016-02-04 14:23:05 +0100

    add login spinner on enter trigger.

--- template.php
+++ template.php
@@ -149,7 +149,7 @@ function latto_odense_form_alter(&$form, &$form_state, $form_id) {
               . '<a href="/user/password">'. t('Glemt kode?').'</a></div>';
       if (variable_get('nemid_button', 0) == 1) {
       /* Make form submit on enter work , with nemid button enabled.*/
-      $form['pass']['#attributes']['onkeypress'][]='if(event.keyCode==13){this.form.submit();}';
+      $form['pass']['#attributes']['onkeypress'][]='if(event.keyCode==13){trigger_loginSpinner();this.form.submit();}';
       $form['nemid']['submit'] = array(
           '#type' => 'submit',
           '#ajax' => array(
