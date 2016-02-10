b5a35d1035b49678c0dafaffe92f65f481f6d706
Tobias F. Petersen <tobsf@hotmail.com>
2016-02-10 12:31:49 +0100

    No Empty titles please :)

--- templates/file--image.tpl.php
+++ templates/file--image.tpl.php
@@ -9,7 +9,7 @@ if(isset($content['file']['#attributes']['title']) && !empty($content['file']['#
 }
 else
 {
-    $content['file']['#title'] = isset($field_kilde['und'][0]['value']) ? "Photo: " . $field_kilde['und'][0]['value'] : '';
+    $content['file']['#title'] = isset($field_kilde['und'][0]['value']) && !empty($field_kilde['und'][0]['value']) ? "Photo: " . $field_kilde['und'][0]['value'] : '';
 }
 hide($content['links']);
 print render($content);
