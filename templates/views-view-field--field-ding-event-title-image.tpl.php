6e64bf27e6d2715abab9b16489a19d021c213576
Tobias F. Petersen <tobsf@hotmail.com>
2016-02-10 09:01:18 +0100

    Moving from field_list_image to field_title_image due to copyright

---
+++ templates/views-view-field--field-ding-event-title-image.tpl.php
@@ -0,0 +1,33 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+if($output != "" &&
+isset($row->field_field_ding_event_list_image[0]['raw']['field_kilde']['und'][0]['value']))
+{
+    $dom = new DOMDocument();
+    $dom->loadHTML($output);
+
+    foreach ($dom->getElementsByTagName('img') as $item) {
+        $photo_source = $row->field_field_ding_event_list_image[0]['raw']['field_kilde']['und'][0]['value'];
+        if($item->hasAttribute('title'))
+        {
+            $old_string =  $item->getAttribute('title');
+            $old_string = $old_string ."\r\n" .t('Photo: ') . $photo_source;
+            $item->setAttribute('title', $old_string);
+        }
+        else
+        {
+           $item->setAttribute('title', t('Photo: ') . $photo_source);
+        }
+        echo $dom->saveHTML();
+        break;
+    }
+}
+else
+{
+    echo $output;
+}
