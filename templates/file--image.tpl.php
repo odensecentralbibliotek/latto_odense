e42138827c07eb308e53a4301c0f163d50b8e748
Tobias F. Petersen <tobsf@hotmail.com>
2016-02-10 12:18:17 +0100

    Add image copyrigth to wysiwig embedded images.

--- templates/file--image.tpl.php
+++ templates/file--image.tpl.php
@@ -1,4 +1,15 @@
 <?php
-
+/*
+ * Add the image copyrigth to the title tag 
+ * to Ensure that we adhere we copyrigth law
+ */
+if(isset($content['file']['#attributes']['title']) && isset($field_kilde['und'][0]['value']))
+{
+    $content['file']['#title'] =  $content['file']['#attributes']['title'] . '\r\n'. "Photo: " . $field_kilde['und'][0]['value'];
+}
+else
+{
+    $content['file']['#title'] = isset($field_kilde['und'][0]['value']) ? "Photo: " . $field_kilde['und'][0]['value'] : '';
+}
 hide($content['links']);
 print render($content);
