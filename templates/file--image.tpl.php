0e7c9287f809c6267ed43666191c693cf5781bd5
Tobias F. Petersen <tobsf@hotmail.com>
2016-02-10 13:18:41 +0100

    linebreaks broken for embedded images.

--- templates/file--image.tpl.php
+++ templates/file--image.tpl.php
@@ -5,7 +5,7 @@
  */
 if(isset($content['file']['#attributes']['title']) && !empty($content['file']['#attributes']['title']) && isset($field_kilde['und'][0]['value']))
 {
-    $content['file']['#title'] =  $content['file']['#attributes']['title'] . ' \r\n '. "Photo: " . $field_kilde['und'][0]['value'];
+    $content['file']['#title'] =  $content['file']['#attributes']['title'] . ' '. "Photo: " . $field_kilde['und'][0]['value'];
 }
 else
 {
