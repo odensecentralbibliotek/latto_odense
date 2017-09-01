f693445a283a5f946f7e2f6223640b5d35abf6e8
Aegir user <aegir@dev.fynbib.dk>
2017-09-01 12:42:36 +0200

    Changed notification text in fbs profile.

--- js/base.js
+++ js/base.js
@@ -5,8 +5,8 @@
     };
 
     $(document).ready(function($) {
-        $("label[for=edit-profile-provider-fbs-field-fbs-phone-notification-und]").html("SMS påmindelser");
-        jQuery("label[for=edit-profile-provider-fbs-field-fbs-mail-notification-und]").html("Email påmindelser");
+        $("label[for=edit-profile-provider-fbs-field-fbs-phone-notification-und]").html("Modtag meddelelser pr. SMS (Bemærk: Fjernes markeringen modtager du ingen typer meddelelser pr. sms fra biblioteket)");
+        jQuery("label[for=edit-profile-provider-fbs-field-fbs-mail-notification-und]").html("Modtag meddelelser pr. e-mail (Bemærk: Fjernes markeringen modtager du ingen typer meddelelser pr. mail fra biblioteket)");
         //prevent download of library videos
          $('video').attr('oncontextmenu','return false');
   
