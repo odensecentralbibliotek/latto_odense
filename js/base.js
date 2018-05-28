d8a8ce21826dd114c1ceca25afc2928302dfba3e
impara <impara1@gmail.com>
2018-05-28 13:26:36 +0200

    Fixed js for personal table

--- js/base.js
+++ js/base.js
@@ -205,7 +205,7 @@
     Drupal.behaviors.ansat_table = {
         attach: function (context, settings) {
           
-          $("#edit-field-bibliotek-tid option:contains('Borgerservice'):selected").parents().find("table").remove();
+          $("#edit-field-bibliotek-tid option:contains('Borgerservice'):selected").parents().find('#views-exposed-form-ansat-page').once().append('<h3>Borgerservice Tlf.: 60811091</h3>');
           
         }
     };
