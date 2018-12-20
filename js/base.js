054f1f231484a3c7695085c49e4d66521fa1cafc
memleakz <dev@dev-VirtualBox>
2018-12-20 11:39:22 +0100

    add view heigth restriction to large image view

--- js/base.js
+++ js/base.js
@@ -224,7 +224,8 @@
                 $('<i class="fa fa-times" aria-hidden="true"></i>').prependTo('.search-overlay--wrapper');
                 $(this).clone().attr("src", cloned).prependTo('.search-overlay--wrapper');
                 $('.search-overlay--wrapper > img').css({
-                    'margin-top': $('.search-overlay--wrapper > img').outerHeight() / 4
+                    'margin-top': $('.search-overlay--wrapper > img').outerHeight() / 4 ,
+                    'max-heigth':'90vh',
                 });
                 $('.search-overlay--wrapper .fa').css({
                     'position': 'absolute',
